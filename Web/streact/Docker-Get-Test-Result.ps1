619b0e8be5e3cba4bf267802be04e346e6fa7d3b
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2019-01-07 22:54:11 +0000

    Support docker test build

---
+++ Web/streact/Docker-Get-Test-Result.ps1
@@ -0,0 +1,44 @@
+﻿param(
+    [parameter(Mandatory=$true)][string]$theImage
+)
+
+Push-Location $PSScriptRoot
+
+$thePath = "$PSScriptRoot/junit.xml"
+
+if(Test-Path -path $thePath)
+{
+    Remove-Item -Force -Path $thePath
+}
+
+# run the image up as a detached container
+Write-Host -Fore Yellow "Run up the image $theImage"
+&docker run -dit $theImage /bin/bash
+
+# get the last run container id
+$containerId = docker ps -alq | % { $_.Split(" ")[0] } 
+Write-Host "Last container id is $containerId"
+
+# Copy the unit test result to host
+&docker cp "$containerId`:/usr/src/app/test/junit.xml" $thePath
+
+# Stop and remove the container 
+Write-Host -Fore Yellow "Stopping and removing $containerId"
+&docker stop $containerId
+&docker rm $containerId
+Write-Host -Fore Yellow "done!"
+
+
+if(Test-Path -path $thePath)
+{
+    Write-Host -Fore Yellow "Result copied to $thePath"
+}
+else
+{
+    Pop-Location
+    throw "failed to get test result!"
+}
+
+Pop-Location
+
+Write-Host -Fore Yellow "All done!"
--- Web/streact/build-and-push.bat
+++ Web/streact/build-and-push.bat
@@ -1,6 +1,8 @@
 SET THE_VERSION=virasana/streact:1.0.0.%1
 echo Building %THE_VERSION% ...
-docker build . -f ./Dockerfile -t virasana/streact:1.0.0.%1
+docker build . -f ./Dockerfile-Run-Tests -t virasana/streact:1.0.0.%1 --no-cache
+powershell .\Docker-Get-Test-Result.ps1 -theImage virasana/streact:1.0.0.%1
+docker build . -f ./Dockerfile-Build-Prod -t virasana/streact:1.0.0.%1 --no-cache --build-arg base_image_name=%THE_VERSION%
 @REM echo Pushing $THE_VERSION ...
 @REM docker push $THE_VERSION
 echo "done!"
