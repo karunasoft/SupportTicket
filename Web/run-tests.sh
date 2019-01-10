b26a13f04c462fe3b7997b91d2fcff92560b6acb
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2019-01-10 16:35:22 +0000

    Fix cobertura

--- Web/run-tests.sh
+++ Web/run-tests.sh
@@ -8,10 +8,6 @@ function RunTests {
 	printf "=T=T=T=> Running Tests on %s \n" $1
 	
 	dotnet test $1 /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --logger "trx;LogFileName=$1results.trx" --results-directory "/TestResults" -c Release
-	
-	dir = $1
-	dir=${dir%*/}
-	dir=${dir##*/} 
 	cp $1/coverage.cobertura.xml /TestResults/$1/coverage.cobertura.xml
 	/root/.dotnet/tools/reportgenerator "-reports:/src/$1/coverage.cobertura.xml" "-targetdir:/TestResults/$1/CoverageReport" "-tag:CodeCoverage" "-reportTypes:htmlInline"
 }
