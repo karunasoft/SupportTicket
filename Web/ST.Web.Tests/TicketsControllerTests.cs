c4db64d3e1c9c83207339807e5ac3eaa80058c45
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2019-01-10 13:31:10 +0000

    STAppServiceTests

--- Web/ST.Web.Tests/TicketsControllerTests.cs
+++ Web/ST.Web.Tests/TicketsControllerTests.cs
@@ -14,21 +14,11 @@ namespace ST.Web.Tests
         [Fact]
         public void GetTicketsReturnsTickets()
         {
-            var tickets = new List<Ticket>()
-            {
-                new Ticket(){TicketId = 1, ProductId = 1, SeverityId = 1, Description = "D1", Problem = "Pr1"},
-                new Ticket(){TicketId = 2, ProductId = 2, SeverityId = 2, Description = "D2", Problem = "Pr2"}
-            };
-
             var fakeStService = A.Fake<ISTAppService<ISTRepo>>();
-            A.CallTo(() => fakeStService.GetActiveTickets()).Returns(tickets);
             var ticketsController = new TicketsController(fakeStService);
             var result = ticketsController.GetTickets();
             Assert.IsType<OkObjectResult>(result);
-            var okObjectResult = ((OkObjectResult) result).Value;
-            Assert.IsType<List<Ticket>>(okObjectResult);
-            var listResult = (List<Ticket>) okObjectResult;
-            Assert.True(listResult.Count == 2);
+            A.CallTo(() => fakeStService.GetActiveTickets()).MustHaveHappened();
         }
     }
 }
